79c440dc2d63558a0a0d5f33f06e6dfa86a2d4e4
DuongMinhHien <duongminhhien14@gmail.com>
2025-11-19 18:26:14 +0700

    config database

---
+++ src/config/sequelize.config.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from "@nestjs/config"
+import { SequelizeModuleOptions } from "@nestjs/sequelize"
+import Sequelize, { Dialect } from "sequelize/lib/sequelize"
+
+export const sequelizeConfig = (configService: ConfigService): SequelizeModuleOptions => ({
+          database: configService.get<string>('DB_NAME'),
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT') || 5432,
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          dialect: configService.get<string>('DB_DIALECT') as Dialect,
+          autoLoadModels: true,
+          synchronize: true,
+})
