a008958cd5b5688d612dd274ae975dd35684df2d
DuongMinhHien <duongminhhien14@gmail.com>
2025-11-19 18:38:11 +0700

    restaurant-dto

---
+++ src/modules/restaurants/dto/create-restaurant.dto.ts
@@ -0,0 +1,54 @@
+import {
+  IsString,
+  IsBoolean,
+  IsOptional,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+} from 'class-validator';
+
+export class CreateRestaurantDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  imageUrl?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  openTime?: string;
+
+  @IsString()
+  @IsOptional()
+  closeTime?: string;
+
+  @IsInt()
+  @IsOptional()
+  rating?: number;
+
+  @IsInt()
+  @IsOptional()
+  status?: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  categorieID: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  userId: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  addressId: number;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
