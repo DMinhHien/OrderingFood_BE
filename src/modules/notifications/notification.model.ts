680470aa8701ca038067aacd8e810e9751ce382d
DuongMinhHien <duongminhhien14@gmail.com>
2025-11-19 18:35:39 +0700

    notification.module

---
+++ src/modules/notifications/notification.model.ts
@@ -0,0 +1,78 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  AutoIncrement,
+  CreatedAt,
+  UpdatedAt,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import { User } from '../users/user.model';
+import { Order } from '../orders/order.model';
+
+@Table({
+  tableName: 'notifications',
+  timestamps: true,
+})
+export class Notification extends Model<Notification> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column({
+    type: DataType.INTEGER,
+  })
+  declare id: number;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  content: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  type: number;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: true,
+    defaultValue: false,
+  })
+  isRead: boolean;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: true,
+  })
+  isActive: boolean;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  receivedId: number;
+
+  @BelongsTo(() => User, { as: 'receiver', foreignKey: 'receivedId' })
+  receiver: User;
+
+  @ForeignKey(() => Order)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  orderId: number;
+
+  @BelongsTo(() => Order)
+  order: Order;
+
+  @CreatedAt
+  declare createdAt: Date;
+
+  @UpdatedAt
+  declare updatedAt: Date;
+}
