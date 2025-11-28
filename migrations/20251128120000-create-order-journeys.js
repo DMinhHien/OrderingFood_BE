be597ea409680504cf2729516d726b8afcb35db3
DuongMinhHien <duongminhhien14@gmail.com>
2025-11-28 22:36:12 +0700

    cập nhật migration cho orderjourney

---
+++ migrations/20251128120000-create-order-journeys.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('order_journeys', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      latitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      longtitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      timeline: {
+        type: Sequelize.TIME,
+        allowNull: true,
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'orders',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('order_journeys');
+  },
+};
